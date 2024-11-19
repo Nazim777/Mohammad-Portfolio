c8c03f58398e3944c439a04c30dcf813ed0ccc6c
Nazim777 <iamnazimhossain@gmail.com>
2024-11-19 21:51:43 +0600

    update 7

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -38,7 +38,8 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         formRef.current,
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "uzjGb2XRbX733mV6S"
       )
       .then(
         () => {
