e13ff6427aa6e5215ab1911c2e703e320f6a1786
Nazim777 <iamnazimhossain@gmail.com>
2024-11-19 16:03:06 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: "/mohammad-portfolio/"
+  base: "/Mohammad-Portfolio/"
 })
