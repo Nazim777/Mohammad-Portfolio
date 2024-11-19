131d76f5d3bf9e4f9998466e0f2f3a985568bd61
Nazim777 <iamnazimhossain@gmail.com>
2024-11-19 16:21:48 +0600

    update 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import { Home, About, Projects, Contact } from "./pages";
 import Navbar from "./components/Navbar";
-
+//
 const App = () => {
   return (
     <main className="bg-slate-300/20">
